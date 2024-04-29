4d6353e78ba0dbb6fc2a43bf006f544802a319c5
Ron Cilva <johndoe@example.com>
2024-04-29 10:32:59 -0700

    stage 02-1

---
+++ back-end/routes/users/users.js
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    res.send("GET called");    
+});
+
+router.post('/', async (req, res) => {
+    res.send("POST called");  
+});
+
+export default router;
+
