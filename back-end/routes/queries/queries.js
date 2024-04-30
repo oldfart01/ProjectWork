af9e878d74a9bd49942c7764305d1d369bfaf320
oldfart01 <ruthroncilva@yahoo.com>
2024-04-30 14:04:45 -0400

    stage 2-6

--- back-end/routes/queries/queries.js
+++ back-end/routes/queries/queries.js
@@ -1,7 +1,9 @@
 import fs from 'fs';
-import express from "express";
+import express from 'express';
+
 const router = express.Router();
 
+// GET endpoint to retrieve the query array from queries.json
 router.get('/', async (req, res) => {
     try {
         const jsonString = fs.readFileSync('queries.json', 'utf8');
@@ -12,21 +14,24 @@ router.get('/', async (req, res) => {
     }
 });
 
-
-router.post('/',  (req, res) => {
-    const { queryArray } = req.body; // Assuming the queryArray is sent in the request body
-    const jsonString = JSON.stringify(queryArray, null, 2);
+// POST endpoint to save the query array to queries.json
+router.post('/', async (req, res) => {
     try {
-       // const { queryArray } = req.body; // Assuming the queryArray is sent in the request body
-        //const jsonString = JSON.stringify(queryArray);
+        const { queryArray } = req.body; // Assuming the queryArray is sent in the request body
+        const jsonString = JSON.stringify(queryArray, null, 2);
         fs.writeFileSync('queries.json', jsonString);
-        console.log('query array saved to queries.json');
+        console.log('Query array saved to queries.json');
         res.status(200).send('Query array saved');
     } catch (err) {
+        console.error(err);
         res.status(500).send('Failed to save query array');
     }
 });
 
-
 export default router;
+
+
+
+
+
 
