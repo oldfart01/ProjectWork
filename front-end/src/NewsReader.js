a9c380658f7aeef5d892e4ce1e1186f14d6f0a44
dhroiden <dhroiden@users.noreply.github.com>
2024-05-01 09:28:18 -0500

    login restricted feature added

--- front-end/src/NewsReader.js
+++ front-end/src/NewsReader.js
@@ -84,8 +84,27 @@ export function NewsReader() {
     setQueryFormObject(selectedQuery);
     setQuery(selectedQuery);
    }
+  
+   function currentUserMatches(user) {
+    if (currentUser) {
+      if (currentUser.user) {
+        if (currentUser.user === user) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 
   function onFormSubmit(queryObject) {
+    if (currentUser === null) {
+      alert("Only logged-in users can create new queries!")
+      return;
+    }
+    if (savedQueries.length >= 3 && currentUserMatches("guest")) {
+      alert("guest users cannot submit new queries once saved query count is 3 or greater!")
+      return;
+    }
     let newSavedQueries = [];
     newSavedQueries.push(queryObject);
     for (let savedQuery of savedQueries) {
@@ -139,6 +158,7 @@ export function NewsReader() {
           <div className="box">
             <span className='title'>Query Form</span>
             <QueryForm
+              currentUser={currentUser}
               setFormObject={setQueryFormObject}
               formObject={queryFormObject}
               submitToParent={onFormSubmit} />
