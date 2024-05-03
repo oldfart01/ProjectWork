62179a447a20afcc806bfcee3f6c5b0dc84c8ea0
dhroiden <dhroiden@users.noreply.github.com>
2024-05-03 11:54:27 -0500

    clean up console log

--- front-end/src/QueryDetails.js
+++ front-end/src/QueryDetails.js
@@ -3,10 +3,6 @@ import React from 'react';
 export function QueryDetails({query}) {
   const queryDetails = [];
 
-  console.log(query);
-  console.log(query!==null);
-  console.log(query.queryName!=='Query11');
-  console.log(query!==null || query.queryName!=='Query11');
   if(query?.queryName!=='Query11') {
     console.log("IN IF");
     queryDetails.push(<span className="sub-title">Query Details</span>);
